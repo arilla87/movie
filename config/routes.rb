050574278e007e4f26f5543a73e7ac16de6d8772
David <arilla94@gmail.com>
2017-05-26 10:01:52 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'statics/about'
 
   get 'peliculas/index'
+  get '/about' => 'statics#about', :as => :about
 
+  root :to => "statics#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
